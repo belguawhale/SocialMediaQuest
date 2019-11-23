ca99fd26ad4e68e20338fc286bc41c523ee598fa
Belgua Whale <belguawhale@gmail.com>
2019-11-23 03:35:11 -0500

    add dev environment

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
-app.get('/', function (req, res) { res.send('<h1>Hello World 2!</h1>'); } );
+const path = require('path');
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + 'index.html'));
+});
+
+app.use('/', router);
 app.listen(3000);
+
+console.log("Listening on port 3000");
