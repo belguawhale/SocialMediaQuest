da68636ff976929f44b805dbb29fef7b30ef633d
Belgua Whale <belguawhale@gmail.com>
2019-11-24 09:11:55 -0500

    add routes

--- app.js
+++ app.js
@@ -7,6 +7,25 @@ router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+router.get('/game.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/game.html'));
+});
+
+router.get('/css/styles.css', function (req, res) {
+    res.sendFile(path.join(__dirname + '/css/styles.css'));
+});
+
+router.get('/css/tabs.css', function (req, res) {
+    res.sendFile(path.join(__dirname + '/css/tabs.css'));
+
+});
+router.get('/js/game.js', function (req, res) {
+    res.sendFile(path.join(__dirname + '/js/game.js'));
+});
+router.get('/js/tabs.js', function (req, res) {
+    res.sendFile(path.join(__dirname + '/js/tabs.js'));
+});
+
 app.use('/', router);
 app.listen(3000);
 
